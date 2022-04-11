aa14bce08aaebd1b26f50e03a5d814429ca1f72a
Roman Karkhachev <karkhachev@cortona3d.com>
2022-04-11 17:38:20 +0300

    add backup/restore of Oracle FRA

--- make_backup.cmd
+++ make_backup.cmd
@@ -2,10 +2,14 @@
 
 set ORACLE_SID=tc
 set TC_VOLUME_DIR=C:\Siemens\volume
-set ORACLE_DATA_DIR=c:\app\oracle\oradata\tc\
+set ORACLE_DATA_DIR=c:\oracle\database\oradata\tc\
+set FRA_DIR=c:\oracle\database\flash_recovery_area\TC\
 set BKP_DIR=C:\app\backup
 set TC_DATA=C:\Siemens\tcdata
-set TC_ROOT=C:\Siemens\Teamcenter10
+set TC_ROOT=C:\Siemens\Teamcenter13
+set tcservicename="Teamcenter Server Manager config1_PoolA"
+set fscservicename="Teamcenter FSC Service FSC_tcserver_user"
+set processservicename="Teamcenter Process Manager"
 set tcdatabckp /p ask="Do you want to make backup of TC_DATA directory(y/n)?"
 
 rem Deleting old backups
@@ -13,8 +17,9 @@ rem TODO Make possible to create defined amount of backups.
 del /q %BKP_DIR%\*
 
 echo Stop TC services...
-sc stop "Teamcenter Server Manager config1_PoolA"
-sc stop "Teamcenter FSC Service FSC_tcserver_user"
+sc stop %tcservicename%
+sc stop %processservicename%
+sc stop %fscservicename%
 
 echo Stop DataBase %ORACLE_SID%
 (
@@ -24,8 +29,9 @@ echo Stop DataBase %ORACLE_SID%
 
 echo Copy database files from %ORACLE_DATA_DIR%
 7z a -aoa -mx1 %BKP_DIR%\db_bkp.7z %ORACLE_DATA_DIR%
-
-
+if exist %FRA_DIR% (
+7z a -aoa -mx1 %BKP_DIR%\fra_bkp.7z %FRA_DIR%
+)
 
 echo Copy TC volume files from %TC_VOLUME_DIR%
 7z a -aoa -mx1 %BKP_DIR%\tc_bkp.7z %TC_VOLUME_DIR%
@@ -41,7 +47,8 @@ echo Start DataBase %ORACLE_SID%
 ) | sqlplus -s -l /nolog
 
 echo Start DataBase %ORACLE_SID%
-sc start "Teamcenter FSC Service FSC_tcserver_user"
-sc start "Teamcenter Server Manager config1_PoolA"
+sc start %fscservicename%
+sc start %processservicename%
+sc start %tcservicename%
 
 pause
--- restore_backup.cmd
+++ restore_backup.cmd
@@ -2,14 +2,19 @@
 
 set ORACLE_SID=tc
 set TC_VOLUME_DIR=C:\Siemens\volume
-set ORACLE_DATA_DIR=c:\app\oracle\oradata\tc\
+set ORACLE_DATA_DIR=c:\oracle\database\oradata\tc\
+set FRA_DIR=c:\oracle\database\flash_recovery_area\TC\
 set BKP_DIR=C:\app\backup
 set TC_DATA=C:\Siemens\tcdata
-set TC_ROOT=C:\Siemens\Teamcenter10
+set TC_ROOT=C:\Siemens\Teamcenter13
+set tcservicename="Teamcenter Server Manager config1_PoolA"
+set fscservicename="Teamcenter FSC Service FSC_tcserver_user"
+set processservicename="Teamcenter Process Manager"
 
 echo Stop TC services...
-sc stop "Teamcenter Server Manager config1_PoolA"
-sc stop "Teamcenter FSC Service FSC_tcserver_user"
+sc stop %tcservicename%
+sc stop %processservicename%
+sc stop %fscservicename%
 
 echo Stop DataBase %ORACLE_SID%
 (
@@ -19,6 +24,9 @@ echo Stop DataBase %ORACLE_SID%
 
 echo Copy database files from %ORACLE_DATA_DIR%
 7z x -aoa %BKP_DIR%\db_bkp.7z -o%ORACLE_DATA_DIR%\..\
+if exist %BKP_DIR%\fra_bkp.7z (
+7z x -aoa %BKP_DIR%\fra_bkp.7z -o%FRA_DIR%\..\ 
+)
 
 
 echo Copy TC volume files from %TC_VOLUME_DIR%
@@ -39,7 +47,8 @@ echo Start DataBase %ORACLE_SID%
 ) | sqlplus -s -l /nolog
 
 echo Start DataBase %ORACLE_SID%
-sc start "Teamcenter FSC Service FSC_tcserver_user"
-sc start "Teamcenter Server Manager config1_PoolA"
+sc start %fscservicename%
+sc start %processservicename%
+sc start %tcservicename%
 
 pause
