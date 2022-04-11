a3212a1696631eeacdf1625f70aa4ee755bd8a7d
Roman Karkhachev <karkhachev@cortona3d.com>
2022-04-11 18:20:57 +0300

    test2

--- check.cmd
+++ check.cmd
@@ -27,7 +27,7 @@ REM --> If error flag set, we do not have admin.
 :UACPrompt
   echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
   set params= %*
-  echo UAC.ShellExecute "cmd.exe", "/c "%~s0"" %params:"="%", ", "runas", 1 >> "%temp%\getadmin.vbs"
+  echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
   "%temp%\getadmin.vbs"
   del "%temp%\getadmin.vbs"
