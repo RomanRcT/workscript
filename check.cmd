53466326f4c30250a0ad9ea7e19f10746194acee
Roman Karkhachev <karkhachev@cortona3d.com>
2022-04-11 18:14:42 +0300

    test

--- check.cmd
+++ check.cmd
@@ -4,18 +4,18 @@ set COUNTER=0
 rem ********** variables **************
 set TC_ROOT=C:\Siemens\Teamcenter10
 set TC_DATA=C:\Siemens\tcdata
-set TNSLISTENER_NAME="OracleOraDB12Home1TNSListener"
+set TNSLISTENER_NAME="OracleOraDB19Home1TNSListener"
 set TC_SERVER_NAME="Teamcenter Server Manager config1_PoolA"
-set dbapassfile="C:\Siemens\Teamcenter10\security\config1_infodba.pwf"
+set dbapassfile="C:\Siemens\Teamcenter13\security\config1_infodba.pwf"
 rem ********** end of variables **************
 
 :: BatchGotAdmin
 :: :-------------------------------------
 REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
-    >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-  ) ELSE (
-  >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
 REM --> If error flag set, we do not have admin.
@@ -23,18 +23,19 @@ REM --> If error flag set, we do not have admin.
   echo Requesting administrative privileges...
   goto UACPrompt
 ) else ( goto gotAdmin )
-:UACPrompt
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-set params= %*
-echo UAC.ShellExecute "cmd.exe", "/c "%~s0"" %params:"="%", ", "runas", 1 >> "%temp%\getadmin.vbs"
 
-"%temp%\getadmin.vbs"
-del "%temp%\getadmin.vbs"
-exit /B
+:UACPrompt
+  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+  set params= %*
+  echo UAC.ShellExecute "cmd.exe", "/c "%~s0"" %params:"="%", ", "runas", 1 >> "%temp%\getadmin.vbs"
+
+  "%temp%\getadmin.vbs"
+  del "%temp%\getadmin.vbs"
+  exit /B
 
 :gotAdmin
-pushd "%CD%"
-CD /D "%~dp0"
+  pushd "%CD%"
+  CD /D "%~dp0"
 :--------------------------------------
 
 
