cc41212a772d5e839dfe4bdc9076070adc4eab3c
Roman Karkhachev <karkhachev@cortona3d.com>
2022-04-11 18:23:15 +0300

    fixed

--- make_backup.cmd
+++ make_backup.cmd
@@ -15,10 +15,10 @@ set tcdatabckp /p ask="Do you want to make backup of TC_DATA directory(y/n)?"
 :: BatchGotAdmin
 :: :-------------------------------------
 REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
-    >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-  ) ELSE (
-  >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
 REM --> If error flag set, we do not have admin.
@@ -26,18 +26,19 @@ REM --> If error flag set, we do not have admin.
   echo Requesting administrative privileges...
   goto UACPrompt
 ) else ( goto gotAdmin )
-:UACPrompt
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-set params= %*
-echo UAC.ShellExecute "cmd.exe", "/c "%~s0"" %params:"="%", ", "runas", 1 >> "%temp%\getadmin.vbs"
 
-"%temp%\getadmin.vbs"
-del "%temp%\getadmin.vbs"
-exit /B
+:UACPrompt
+  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+  set params= %*
+  echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+  "%temp%\getadmin.vbs"
+  del "%temp%\getadmin.vbs"
+  exit /B
 
 :gotAdmin
-pushd "%CD%"
-CD /D "%~dp0"
+  pushd "%CD%"
+  CD /D "%~dp0"
 :--------------------------------------
 rem Deleting old backups
 rem TODO Make possible to create defined amount of backups.
--- restore_backup.cmd
+++ restore_backup.cmd
@@ -14,10 +14,10 @@ set processservicename="Teamcenter Process Manager"
 :: BatchGotAdmin
 :: :-------------------------------------
 REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
-    >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-  ) ELSE (
-  >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
 REM --> If error flag set, we do not have admin.
@@ -25,18 +25,19 @@ REM --> If error flag set, we do not have admin.
   echo Requesting administrative privileges...
   goto UACPrompt
 ) else ( goto gotAdmin )
-:UACPrompt
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-set params= %*
-echo UAC.ShellExecute "cmd.exe", "/c "%~s0"" %params:"="%", ", "runas", 1 >> "%temp%\getadmin.vbs"
 
-"%temp%\getadmin.vbs"
-del "%temp%\getadmin.vbs"
-exit /B
+:UACPrompt
+  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+  set params= %*
+  echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+  "%temp%\getadmin.vbs"
+  del "%temp%\getadmin.vbs"
+  exit /B
 
 :gotAdmin
-pushd "%CD%"
-CD /D "%~dp0"
+  pushd "%CD%"
+  CD /D "%~dp0"
 :--------------------------------------
 echo Stop TC services...
 sc stop %tcservicename%
