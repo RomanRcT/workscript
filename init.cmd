2d2d264a07eddc39cf78f9dffccc1603dd619b8d
Roman Karkhachev <karkhachev@cortona3d.com>
2022-12-21 12:13:26 +0100

    support TC14

--- init.cmd
+++ init.cmd
@@ -9,14 +9,14 @@ set TC_SERVER_NAME=xxxxxxxxx
 set FMS_NAME=xxxxxxxxxx
 rem ==============
 set AWC=yes
-set "TC_ROOT=C:\Siemens\Teamcenter13"
+set "TC_ROOT=C:\Siemens\Teamcenter14"
 set "TC_DATA=C:\Siemens\tcdata"
 set PROCESS_NAME="Teamcenter Process Manager"
-set "dbapassfile=C:\Siemens\Teamcenter13\security\config1_infodba.pwf"
+set "dbapassfile=C:\Users\user\Documents\config1_infodba.pwf"
 set ORACLE_SID=tc
 set "TC_VOLUME_DIR=C:\Siemens\volume\"
-set ORACLE_DATA_DIR=c:\oracle\database\oradata\tc\
-set FRA_DIR=c:\oracle\database\flash_recovery_area\TC\
+set ORACLE_DATA_DIR=c:\app\oracle\oradata\tc\
+set FRA_DIR=c:\app\oracle\flash_recovery_area\TC\
 set BKP_DIR=C:\app\backup
 rem ********** end of variables **************
 :start
